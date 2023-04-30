f7c2ef5e192f1750e322ed9fb53ea70811f4d9ed
Ankur Raikwar <ankur.r.name@gmail.com>
2023-04-30 20:46:57 +0530

    ready for release

--- app/src/main/java/app/lawnchair/lawnicons/Data.java
+++ app/src/main/java/app/lawnchair/lawnicons/Data.java
@@ -35,7 +35,8 @@ public class Data extends Thread {
         contributors.add(new Contributor("RedSkulxHYDRA", ResourcesCompat.getDrawable((context.getResources()), R.drawable.avatar_redskul, null), false));
         contributors.add(new Contributor("Sepehr", ResourcesCompat.getDrawable((context.getResources()), R.drawable.avatar_sepehr, null), false));
         contributors.add(new Contributor("Jorge da Silva", ResourcesCompat.getDrawable((context.getResources()), R.drawable.avatar_jorge, null), false));
-        contributors.add(new Contributor("Abdul Aziz Shakib", ResourcesCompat.getDrawable((context.getResources()), R.drawable.avatar_shakib, null), true));
+        contributors.add(new Contributor("Abdul Aziz Shakib", ResourcesCompat.getDrawable((context.getResources()), R.drawable.avatar_shakib, null), false));
+        contributors.add(new Contributor("Ankur Raikwar", "Icon Artist and Developer", ResourcesCompat.getDrawable(context.getResources(), R.drawable.avatar_ankur, null)));
     }
 
     public static ArrayList<Contributor> getContributors() {
@@ -43,12 +44,24 @@ public class Data extends Thread {
     }
 
     private static void loadLatestIcons() {
-        latestIcons.add(new Icon("AOSP Enhancer", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_aosp_enhancer, null)));
-        latestIcons.add(new Icon("Audible", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_audible, null)));
-        latestIcons.add(new Icon("Pixelcut", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_pixelcut, null)));
-        latestIcons.add(new Icon("OnStream", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_onstream, null)));
-        latestIcons.add(new Icon("Internshala", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_internshala, null)));
-
+        latestIcons.add(new Icon("Arts & Culture", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_arts_culture, null)));
+        latestIcons.add(new Icon("BikeWale", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_bikewale, null)));
+        latestIcons.add(new Icon("Blogger", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_blogger, null)));
+        latestIcons.add(new Icon("CarWale", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_carwale, null)));
+        latestIcons.add(new Icon("DigiLocker", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_digilocker, null)));
+        latestIcons.add(new Icon("Dominos", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_dominos, null)));
+        latestIcons.add(new Icon("Enpass", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_enpass, null)));
+        latestIcons.add(new Icon("GoogleGo", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_google_go, null)));
+        latestIcons.add(new Icon("Unison", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_intel_unison, null)));
+        latestIcons.add(new Icon("IRCTC Rail", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_irctc_rail, null)));
+        latestIcons.add(new Icon("Microsoft 365", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_microsoft_365, null)));
+        latestIcons.add(new Icon("PhotoScan", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_photoscan_google, null)));
+        latestIcons.add(new Icon("SpaceDesk", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_spacedesk, null)));
+        latestIcons.add(new Icon("WiproNext", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_wipro_next, null)));
+        latestIcons.add(new Icon("YouTube Kids", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_youtube_kids, null)));
+        latestIcons.add(new Icon("YouTube Studio", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_youtube_studio, null)));
+        latestIcons.add(new Icon("Wallpapers", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_google_wallpaper, null)));
+        latestIcons.add(new Icon("Where is My Train", ResourcesCompat.getDrawable(context.getResources(), R.drawable.themed_icon_whereismytrain, null)));
         //Request Icons
         //latestIcons.add(new Icon("Request More", ResourcesCompat.getDrawable(context.getResources(), R.drawable.ic_request, null), true));
     }
